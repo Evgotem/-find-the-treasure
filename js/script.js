f23ee9dbd3e018759c54a177582d1e53e0b03921
Evgotem <57010853+Evgotem@users.noreply.github.com>
2021-01-24 15:27:11 +0400

    додела игру, переписал скрыпты, сделал вывод кнопки, исчезание блоков, сбрасывание счетчика

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 // коллекция блоков
 let blocksQuantity = document.querySelectorAll(".block");
-console.log(blocksQuantity.length);
+let hideTarget = document.querySelector('.hide-target');
+
+
 
 // генерируем случайный индекс блока
 let getRandomNumber = function () {
   return Math.floor(Math.random() * blocksQuantity.length);
 };
-console.log(getRandomNumber());
-
-// let target = blocksQuantity[getRandomNumber()];
+let randomNumber = getRandomNumber();
+let target = blocksQuantity[randomNumber];
+// генерируем кнопку скрытия цели
+hideTarget.innerHTML = 'Показать цель';
+hideTarget.onclick = () => {
+   target.classList.toggle('active');
+   if (hideTarget.innerHTML == 'Показать цель') {
+      hideTarget.innerHTML = 'Скрыть цель';
+   } else {
+      hideTarget.innerHTML = 'Показать цель';
+   }
+}
 
 // создаем счетчик попыток
 let counter = document.querySelector("span");
-let counterValue = 7;
+let counterValue = 12;
 
 counter.innerHTML = counterValue;
 
@@ -21,14 +32,16 @@ for (let i = 0; i < blocksQuantity.length; i++) {
   blocksQuantity[i].onclick = function () {
     counterValue--;
     counter.innerHTML = counterValue;
+    if (blocksQuantity[i] == target) {
+       alert("Вы победили!");
+       location.reload();
+    } else if (blocksQuantity[i] != target) {
+      blocksQuantity[i].classList.add('hiden');
+    }
     if (counterValue < 0) {
-      alert("Вы проиграли");
+      alert("Проигрышь :(");
       location.reload();
     }    
   };
-  // ANCHOR починить победу
-  blocksQuantity[getRandomNumber()].onclick = function(){
-    alert("Вы победили!");
-    location.reload();
-  }
+ 
 }
