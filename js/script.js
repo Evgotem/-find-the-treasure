e4daeeb796b6e257b3c06f3bf6898e66c4012eec
Evgotem <57010853+Evgotem@users.noreply.github.com>
2021-01-24 16:36:42 +0400

    js немного рефакторинга

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // коллекция блоков
 let blocksQuantity = document.querySelectorAll(".block");
 let hideTarget = document.querySelector('.hide-target');
-
+let input = document.querySelector('.input');
 
 
 // генерируем случайный индекс блока
@@ -11,41 +11,44 @@ let getRandomNumber = function () {
 let randomNumber = getRandomNumber();
 let target = blocksQuantity[randomNumber];
 // генерируем кнопку скрытия цели
-hideTarget.innerHTML = 'Показать цель';
+hideTarget.innerHTML = 'Показать клад';
 hideTarget.onclick = () => {
    target.classList.toggle('active');
-   if (hideTarget.innerHTML == 'Показать цель') {
-      hideTarget.innerHTML = 'Скрыть цель';
+   if (hideTarget.innerHTML == 'Показать клад') {
+      hideTarget.innerHTML = 'Скрыть клад';
    } else {
-      hideTarget.innerHTML = 'Показать цель';
+      hideTarget.innerHTML = 'Показать клад';
    }
 }
 
 // создаем счетчик попыток
-let counter = document.querySelector("span");
-let counterValue = 12;
-
+let counter = document.querySelector(".counter");
+let counterValue = +input.value;
 counter.innerHTML = counterValue;
+input.onchange = () => {
+   counter.innerHTML = +input.value;
+   counterValue = +input.value;
+}
 
 // при ошибке счетчик уменьшается на 1, и перезагружает страницу при проигрыше
 for (let i = 0; i < blocksQuantity.length; i++) {
+   function areYouWin  (x) {
+      alert(x);
+      location.reload();
+   }
   blocksQuantity[i].onclick = function () {
     counterValue--;
     counter.innerHTML = counterValue;
-    if (blocksQuantity[i] == target) {
+    if (counterValue < 0 && blocksQuantity[i] != target) {
       target.classList.toggle('active');
-      setTimeout(youWin, 100);
-      function youWin  () {
-         alert("Вы победили!");
-         location.reload();
-      }
+      areYouWin('Вы проиграли :(');
+    }  else if (blocksQuantity[i] == target) {
+      target.classList.toggle('active');
+      areYouWin('Вы победили!!');
     } else if (blocksQuantity[i] != target) {
       blocksQuantity[i].classList.add('hiden');
-    }
-    if (counterValue < 0) {
-      alert("Проигрышь :(");
-      location.reload();
-    }    
+    }  
   };
  
 }
+
